bd6402a7ba435efba42d642d0600fef4a6934236
jzero92@daum.net <jzero92@daum.net>
2021-03-21 16:58:38 +0900

    sdfds

--- solution/find_primeNum4List.py
+++ solution/find_primeNum4List.py
@@ -31,27 +31,70 @@ def solution2(nums):
     return answer
 
 
+def getBit(num):
+    cnt = 0
 
+    while num != 0:
+        num = num & (num -1)
+        cnt += 1
+    
+    return cnt
 
 
 def solution(nums):
+    answer = 0
+
+    a_size = len(nums)
+    nums.sort()
+
+    maxSum = 0
+    for i in nums[-3:]:
+        maxSum += i
+
+    primes = [True for i in range(maxSum+1)]
+    
+    for i in range(2, maxSum+1):
+        if primes[i]:
+            j = 2
+            while i * j <= maxSum:
+                primes[i * j] = False
+                j += 1
+    for i in range(a_size):
+        for j in range(i+1, a_size):
+            for k in range(j+1, a_size):
+                tmp = nums[i] + nums[j] + nums[k]
+                if primes[tmp]:
+                    answer += 1
+    # for i in range(2**a_size):
+    #     if getBit(i) != 3:
+    #         continue
+    #     flag = bin(i)[2:].zfill(a_size)
+    #     tmp = sum([nums[j] for j in range(a_size) if flag[j] == '1'])
+    #     if primes[tmp]:
+    #         answer += 1
+
+    return answer
+
+print(solution([1,2,3,4,8,10,11,13,15,19,20,21,22,23,24,25,27,100,42]))
+print(solution([1,2,3,4,8]))
+
+def solution3(nums):
     answer = 0
     primeCnt = 0 
 
     powerSet = []
 
     a_size = len(nums)
-    print(2**a_size)
-    print(bin(4)[2:].zfill(a_size))
-
-    start = time.time()
     for i in range(2**a_size):
         flag = bin(i)[2:].zfill(a_size)
         subset = [nums[j] for j in range(a_size) if flag[j] == '1']
         if len(subset) == 3:
-            powerSet.append(sum(subset))
+            tmp = sum(subset)
+            if tmp % 2 == 0 or tmp % 3 == 0 or tmp % 5 == 0:
+                continue
+            else:
+                powerSet.append(tmp)
     
-    print("WorkingTime: {} sec".format(time.time() - start))
     maxNum = max(powerSet)
 
     primes = [True for i in range(maxNum+1)]
@@ -63,11 +106,7 @@ def solution(nums):
                 primes[i * j] = False
                 j += 1
 
-    primeList = [ i for i in range(2, maxNum+1) if primes[i]]    
-
     for i in range(len(powerSet)):
-        if powerSet[i] in primeList:
+        if primes[powerSet[i]] :
             answer += 1
     return answer
-
-print(solution([1,2,3,4,8,10,11,13,15,19,20,21,22,23,24,25,27,100,42]))
